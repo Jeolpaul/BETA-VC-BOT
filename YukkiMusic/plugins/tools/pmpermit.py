27a13a112767dafed02e3ea4ab4b6d3bd6cc9ea8
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-08-30 19:05:41 +0530

    Create pmpermit.py

---
+++ YukkiMusic/plugins/tools/pmpermit.py
@@ -0,0 +1,86 @@
+import asyncio
+from pyrogram import Client
+from helpers.filters import command
+from config import OWNER_ID, PMPERMIT, BOT_NAME
+from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from assistant import client as USER
+
+PMSET =True
+pchats = []
+
+lel = app.username
+SUDO_USERS = OWNER_ID
+
+@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
+async def pmPermit(client: USER, message: Message):
+    if PMPERMIT == "ENABLE":
+        if PMSET:
+            chat_id = message.chat.id
+            if chat_id in pchats:
+                return
+            await USER.send_message(
+                message.chat.id,
+                f"ʜᴇʏ {message.from_user.mention()},\nᴛʜɪs ɪs [{bn}](t.me/{lel}) ʙᴏᴛ ᴀssɪsᴛᴀɴᴛ ᴀᴄᴄᴏᴜɴᴛ.\n\nᴅᴏɴ'ᴛ ᴛʀʏ ᴛᴏ sᴘᴀᴍ ʜᴇʀᴇ ᴇʟsᴇ ʏᴏᴜ ᴡɪʟʟ ɢᴇᴛ ꜰᴜ*ᴋᴇᴅ ʙʏ [ImRishmika](t.me/ImRishmika).\n",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "❤️‍🩹 MUSIC BOT ❤️‍🩹", url=f"https://t.me/{app.USERNAME}"
+                    ),
+                    InlineKeyboardButton(
+                        "💖 SOURCE CODE 💖", url="https://github.com/RishBroPromax/Cat-X-Girl-Music"
+                    )
+                ],[ 
+                    InlineKeyboardButton(
+                        "💞 BOT OWNER 💞", user_id=OWNER_ID"
+                    )]
+            ]
+        ),
+
+    )
+            return
+
+
+@Client.on_message(filters.command(["pm", "pmpermit"]))
+async def bye(client: Client, message: Message):
+    if message.from_user.id in SUDO_USERS:
+        global PMSET
+        text = message.text.split(" ", 1)
+        queryy = text[1]
+        if queryy == "on":
+            PMSET = True
+            await message.reply_text("» ᴘᴍ ᴘᴇʀᴍɪᴛ ᴇɴᴀʙʟᴇᴅ.")
+            return
+        if queryy == "off":
+            PMSET = None
+            await message.reply_text("» ᴘᴍ ᴘᴇʀᴍɪᴛ ᴅɪsᴀʙʟᴇᴅ.")
+            return
+
+@USER.on_message(filters.text & filters.private & filters.me)        
+async def autopmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if not chat_id in pchats:
+        pchats.append(chat_id)
+        await message.reply_text("» ᴀᴜᴛᴏ ᴀᴘᴘʀᴏᴠᴇᴅ ᴛᴏ ᴘᴍ.")
+        return
+    message.continue_propagation()    
+    
+@USER.on_message(filters.command("a", ["!", ".", ""]) & filters.me & filters.private)
+async def pmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if not chat_id in pchats:
+        pchats.append(chat_id)
+        await message.reply_text("» ᴀᴘᴘʀᴏᴠᴇᴅ ᴛᴏ ᴘᴍ.")
+        return
+    message.continue_propagation()    
+    
+
+@USER.on_message(filters.command("da", ["!", ".", ""]) & filters.me & filters.private)
+async def rmpmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if chat_id in pchats:
+        pchats.remove(chat_id)
+        await message.reply_text("» ᴅɪsᴀᴘᴘʀᴏᴠᴇᴅ ᴛᴏ ᴘᴍ.")
+        return
+    message.continue_propagation()
