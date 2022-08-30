bc624112805f03acd8175e48e9ae1e21dc5238a1
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-08-30 16:41:30 +0530

    Update inlinequery.py

--- YukkiMusic/utils/inlinequery.py
+++ YukkiMusic/utils/inlinequery.py
@@ -19,6 +19,7 @@ answer.extend(
             description="Get The Source Of Me",
             thumb_url="https://graph.org/file/bb1ebd7c68d600dae4600.jpg",
             input_message_content=InputTextMessageContent("BETA MUSIC IS A 100% Clone Of Yukki | https://github.com/Jeolpaul/BETA-MUSIC-OFFICIAL | ALL CREDITS GOES TO YUKKI"),
+
     if search_query == "":
         await client.answer_inline_query(
             query.id,
