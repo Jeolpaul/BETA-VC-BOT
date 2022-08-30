65cc681c0f12759bf33e894a832b7b2194b9cba6
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-08-30 17:07:02 +0530

    Update inlinequery.py

--- YukkiMusic/utils/inlinequery.py
+++ YukkiMusic/utils/inlinequery.py
@@ -28,6 +28,7 @@ answer.extend(
     [
         InlineQueryResultArticle(
             title="SEARCH ANY QUERY HERE🔍",
+            thumb="https://telegra.ph//file/e937426b58e31a881c25f.jpg",
             input_message_content=InputTextMessageContent("/inlinehelp"),
         ),
     ]
