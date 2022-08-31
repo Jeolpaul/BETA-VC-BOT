3fea0342a5109d20205fc9ed544f2e5ce6bc8740
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-08-31 15:48:19 +0530

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -33,7 +33,7 @@ class YukkiBot(Client):
         self.id = get_me.id
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, f"MusicBot Started as {self.name}"
+                config.LOG_GROUP_ID, "MusicBot Started as {self.name}"
             )
         except:
             LOGGER(__name__).error(
