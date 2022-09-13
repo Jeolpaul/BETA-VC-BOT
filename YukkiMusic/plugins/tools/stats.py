9ec045de8402b027da036d6687691f047becea89
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-09-13 19:23:47 +0530

    Update stats.py

--- YukkiMusic/plugins/tools/stats.py
+++ YukkiMusic/plugins/tools/stats.py
@@ -48,6 +48,7 @@ STATS_COMMAND = get_command("STATS_COMMAND")
 @app.on_message(
     filters.command(STATS_COMMAND)
     & filters.group
+    & filters.private
     & ~filters.edited
     & ~BANNED_USERS
 )
@@ -66,6 +67,7 @@ async def stats_global(client, message: Message, _):
 @app.on_message(
     filters.command(GSTATS_COMMAND)
     & filters.group
+    & filters.private
     & ~filters.edited
     & ~BANNED_USERS
 )
