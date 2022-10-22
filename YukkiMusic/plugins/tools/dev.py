6cc0762e9345b7d1c8da316b7f19a9d6798ccd81
Itz Me JEOL <102511853+Jeolpaul@users.noreply.github.com>
2022-10-22 10:34:47 +0530

    Update dev.py

--- YukkiMusic/plugins/tools/dev.py
+++ YukkiMusic/plugins/tools/dev.py
@@ -1,1 +1,17 @@
-
+@app.on_callback_query()
+async def callback(bot, msg):
+   data = msg.data
+   if data == "dev":
+         users = await get("https://api.github.com/repos/Jeolpaul/MUSICBYPASS/contributors")
+         list_of_users = ""
+         count = 1
+         for user in users:
+             list_of_users += (f"**{count}.** [{user['login']}]({user['html_url']})\n")       
+             count += 1 
+         await msg.message.edit(
+             text=SOURCE.format(dev=list_of_users),
+             reply_markup=InlineKeyboardMarkup( [[
+                  InlineKeyboardButton("👨‍💻 Developer", user_id=5558249587),
+                  ]]
+                  )
+             )
